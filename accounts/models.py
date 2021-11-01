27d13abcdd0602f83f3f6c1336db25a48264305d
asketsystem <asketsystem1@gmail.com>
2021-11-01 16:06:19 +0000

    create a custom user model by subclassing Django's AbstractUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translate import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-from .managers import CustomUserManager
+
 
 import uuid
+import datetime
 
-class CustomerUser(AbstractUser):
+class CustomUser(AbstractUser):
 
     username = None
     uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -15,13 +16,12 @@ class CustomerUser(AbstractUser):
     last_name = models.CharField(_("last name"), max_length=150, blank=False)
     date_of_birth = models.DateField(_("date of birth"), max_length=150, blank=False)
     verified = models.BooleanField(_("verified"), default=False)
-
+    
+    
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
-
--- django_wallets/settings.py
+++ django_wallets/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'wallets.apps.WalletsConfig'
 ]
 
-AUTH_USER_MODEL = "accounts.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'django_wallets.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
