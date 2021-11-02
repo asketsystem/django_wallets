88915d8d51fbb1b4c24722810741bb26d1294baa
asketsystem <asketsystem1@gmail.com>
2021-11-02 15:41:46 +0000

    Adding the registration url to the app:

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from django.conf.urls import url
+from django.urls import path
+from .views import register
+app_name = "accounts"
 
+urlpatterns = [
+    path('register/', register, name="register"),
+]
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
